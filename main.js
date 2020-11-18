e801efa7c14c6d9aa7e8fd2442a790d28ba34fa9
Shane meek <37853304+KrazyCrave@users.noreply.github.com>
2020-11-17 22:28:35 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,136 @@
+const db = require('quick.db');
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = 'd!';
+
+const { token, default_prefix } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+const fs = require('fs');
+const { isNull } = require('util');
+
+const config = require('./config.json');
+
+
+client.commands = new Discord.Collection();
+client.items = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+
+}
+
+
+const itemsFiles = fs.readdirSync('./items/').filter(file => file.endsWith('.js'))
+for(const file of itemsFiles){
+    const items = require(`./items/${file}`);
+
+    client.items.set(items.name, items);
+
+}
+
+
+
+
+client.once('ready', () => {
+    console.log('Dagger is online!');
+
+    
+
+    client.user.setActivity('d!help', { type: "STREAMING"})
+        .then(presence => console.log(`Activity set to ${presence.activites[0].name}`))
+        .catch(console.error);
+});
+
+
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+
+   
+
+    client.on('guildMemberAdd', member => {
+    if(member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+})
+
+client.on('guildMemberRemove', member => {
+    if(member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+
+    
+})
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    let prefix = await db.get(`prefix_${message.guild.id}`);
+    if(prefix === null) prefix = default_prefix;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.commands.has(command));
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+client.on("message", async message => {
+
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    let prefix = await db.get(`prefix_${message.guild.id}`);
+    if(prefix === null) prefix = default_prefix;
+
+    if(message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+        const command = args.shift().toLowerCase();
+
+        if(!client.items.has(command));
+
+
+        try {
+            client.items.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
+
+
+    
+});
+
+client.login('NzcyNTMzNjA4MjA0MDc1MDE5.X58D_Q.IiaMmsoNMbyrAtWA4PT269T8D0w');
+
