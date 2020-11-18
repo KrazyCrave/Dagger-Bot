74b42958211dedd74db4e5af4a711e2f1c516b5b
Shane meek <37853304+KrazyCrave@users.noreply.github.com>
2020-11-17 22:09:08 -0500

    Create ace.js

---
+++ Commands/ace.js
@@ -0,0 +1,10 @@
+module.exports = { 
+    name: 'ace',
+    description: "command to tell us Casey is sus",
+    async run(client, message, args){
+        message.channel.send('<@322926463101960192> very sus');
+    
+    }
+}
+
+//command for a friend
